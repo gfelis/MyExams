cab616ecbe933b1862a8ea7fa4cee4c47961f650
Sergimation <Sergimation@github.com>
2021-01-09 17:38:27 +0100

    before merge

--- exams/models.py
+++ exams/models.py
@@ -1,18 +1,14 @@
 from django.db import models
 
-class GetLocation(models.Model):
-    port = models.IntegerField()
-    host = models.CharField(default="127.0.0.1", max_length=20)
-    bind_key = models.CharField(max_length=20)
 
-    def __str__(self):
-        return f"{self.host}:{self.port}/{self.bind_key}"
 
 class Exam(models.Model):
 	title = models.CharField(max_length=250)
 	description = models.CharField(max_length=250)
 	date_start = models.DateField()
 	date_finish= models.DateField()
+	#location = models.ForeignKey(ExamLocation, on_delete=models.DO_NOTHING)
+	questions = models.FileField(upload_to='exam_questions', default= 'No Questions')
 	
 	def __str__(self):
 		return self.title
